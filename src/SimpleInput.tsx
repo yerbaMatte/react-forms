78558150b1c44c23d1d722d4912c86c565aae1d9
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-03-25 08:58:40 +0100

    form validation

--- src/SimpleInput.tsx
+++ src/SimpleInput.tsx
@@ -1,7 +1,9 @@
 import { SyntheticEvent, useRef, useState } from 'react';
 
 const SimpleInput = () => {
+  const nameInputRef = useRef<HTMLInputElement>(null);
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
 
   const nameInputChangeHandler = (
     event: React.ChangeEvent<HTMLInputElement>
@@ -11,13 +13,30 @@ const SimpleInput = () => {
 
   const formSubmissionHandler = (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    const enteredValue = nameInputRef.current.value;
+    setEnteredName('');
   };
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler} />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        <p>Name must be empty</p>
       </div>
       <div className='form-actions'>
         <button>Submit</button>
